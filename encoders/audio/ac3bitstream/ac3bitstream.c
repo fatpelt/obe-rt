af36d0872cf1fd9e9c6961e438c3431ecfdf57fc
Steven Toth <stoth@ltnglobal.com>
2018-02-19 15:52:17 -0500

    ac3: Increment the frame time regardless of the overall stream quality. Add comments. Cherry picked mostly from an earlier revert commit f5d26e74.

--- encoders/audio/ac3bitstream/ac3bitstream.c
+++ encoders/audio/ac3bitstream/ac3bitstream.c
@@ -155,6 +155,17 @@ static void * detector_callback(void *user_context,
                 payload);
 #endif
 
+	/* Increment by 2880 90KHz clock ticks, expressed in 27MHz (SCR rate).
+  	 * Or, 32ms of audio, in each packet, matching all of the other broadcaster
+  	 * streams. We need to do this regardless if whether the incoming AC3BITSTREAM
+  	 * frame is good or bad. If the frame fails to validate, we end up warping
+  	 * time too far from the PCR for downstream decoders. Warping time
+  	 * results in downstream decoders no longer playing audio after a "period"
+  	 * of time, for example when the warp becomes more than 10 seconds.
+  	 */
+#define PTS_TICKS_TO_27MHZ(n)  ((n) * 300)
+ 	cur_pts += PTS_TICKS_TO_27MHZ(2880);
+
         if (datatype != 1 /* AC3 */) {
                 fprintf(stderr, "[AC3] Detected SMPTE337 datamode %d, we don't support it.", datamode);
 		return 0;
@@ -240,9 +251,6 @@ static void *start_encoder_ac3bitstream(void *ptr)
 
 		obe_raw_frame_t *frm = encoder->queue.queue[0];
 		pthread_mutex_unlock(&encoder->queue.mutex);
-
-		/* Cache the latest PTS */
-		cur_pts = frm->pts;
 
 #if LOCAL_DEBUG
 		/* Send any audio to the AC3 frame slicer.
